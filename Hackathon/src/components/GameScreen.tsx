ba141948960a7f7df746aadfb45d123ca30100e5
LestarDev <98614691+LestarDev@users.noreply.github.com>
2022-10-20 12:18:49 +0200

    add layout

--- Hackathon/src/components/GameScreen.tsx
+++ Hackathon/src/components/GameScreen.tsx
@@ -1,15 +1,13 @@
-import { useEffect, useRef } from "react";
 import Asteroid from "./pages/asteroid";
-import useCurrency from "../hook/useCurrency";
-import Greenhouse from "./pages/greenhouse";
+import Layout from "./pages/layout/layout";
 
 
 const GameScreen = () => {
-    const xd = useCurrency()
+    
     
     return (
         <div>
-        <Asteroid />
+            <Layout />
         </div>
     )
 }
