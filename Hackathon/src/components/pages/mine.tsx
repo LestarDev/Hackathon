8db238e4f7fbbf004d062d3634052debff6fa45d
kostekpl987 <38163890+kostekpl987@users.noreply.github.com>
2022-10-20 15:14:01 +0200

    costam14niedziala_bartkowi

--- Hackathon/src/components/pages/mine.tsx
+++ Hackathon/src/components/pages/mine.tsx
@@ -3,6 +3,10 @@ import copperOre from './../../assets/copperOre.png'
 import ironOre from './../../assets/ironOre.png'
 import goldOre from './../../assets/goldOre.png'
 import upgrade0 from './../../assets/upgrade0.png'
+//import copperIcon from "./../../../assets/big_miedz_item.png"
+//import ironIcon from "./../../../assets/big_zelazo_item.png"
+//import goldIcon from "./../../../assets/big_zloto_item.png"
+import './../../components/pages/layout/layout.css'
 const Mine = () => {
 
     function copperUpgrades():number{
@@ -24,18 +28,39 @@ const Mine = () => {
                     
                 </div>
                 <img src={copperOre} className='copperOre' alt='copperOre'/>
+
+                <div className='upgrade1'>
+                    <p>UPGRADE</p>
+                </div>
+        
+
+                <div className='odblokuj1'><p>odblokuj</p></div>
                 <div className='gate1'></div>
+
                 <div className='floor1'></div>
+
                 <img src={ironOre} className='ironOre' alt='ironOre'/>
+
+
+
+                <div className='upgrade2'>
+                    <p>UPGRADE</p>
+                </div>
+
+
+                <div className='odblokuj2'><p>odblokuj</p></div>
                 <div className='gate2'></div>
                 <div className='floor2'></div>
                 <img src={goldOre} className='goldOre' alt='goldOre'/>
-                <div className='leftWall'>
-                    <img className='upgrade1' src={upgrade0} alt='upgradeCopper'/>
-                    <img className='upgrade2' src={upgrade0} alt='upgradeIron'/>
-                    <img className='upgrade3' src={upgrade0} alt='upgradeGold'/>
+                                <div className='leftWall'>
+                                    <img className='level1' src={upgrade0} alt='upgradeCopper'/>
+                                    <img className='level2' src={upgrade0} alt='upgradeIron'/>
+                                    <img className='level3' src={upgrade0} alt='upgradeGold'/>
+                                </div>
+                <div className='upgrade3'>
+                    <p>UPGRADE</p>
                 </div>
-                <div className='floor'></div>
+    <div className='floor'></div>
 
         </div>
     )
