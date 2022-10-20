c89cb57cfe3ef1da9730a33e0195dd0f370b6f8f
LestarDev <98614691+LestarDev@users.noreply.github.com>
2022-10-20 09:23:54 +0200

    create player

--- Hackathon/src/components/GameScreen.tsx
+++ Hackathon/src/components/GameScreen.tsx
@@ -1,6 +1,35 @@
+import { useEffect, useRef } from "react";
+import Player from "./client/Player";
+
 const GameScreen = () => {
+    const player = new Player();
+    const ScreenWidth = window.innerWidth;
+    const ScreenHeight = window.innerHeight;
+
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+    let x = 10;
+
+    useEffect(() => {
+        const render = () => {
+            const canvas = canvasRef.current!;
+            const ctx = canvas.getContext("2d")!;
+            ctx.fillStyle = "green";
+            ctx.fillRect(x, x, 150, 100);
+            x+=10;
+            if(x<ScreenWidth){
+                player.Iron+=1;
+                requestAnimationFrame(render);
+            }
+                
+        }
+        render();
+    }, [])
+
     return (
-        <div></div>
+        <canvas ref={canvasRef} height={ScreenHeight} width={ScreenWidth}>
+            {player.Iron}
+        </canvas>
     )
 }
 
